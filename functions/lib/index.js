ff6ef8a44660cf223c42e61566a1cac881ad6088
vnspriyanshu <priyanshu.singh@uxdlab.com>
2026-06-22 19:04:50 +0530

    chore: configure firebase deployment, update functions to Node 20 and v1 API

--- functions/lib/index.js
+++ functions/lib/index.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.provisionTechnician = exports.onTicketUpdate = exports.autoAssignTicket = void 0;
-const functions = require("firebase-functions");
+const functions = require("firebase-functions/v1");
 const admin = require("firebase-admin");
 const firestore_1 = require("firebase-admin/firestore");
 admin.initializeApp();
